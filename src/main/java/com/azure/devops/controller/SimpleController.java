6f80aa8ce7bd8b9a218a8db7cdb01f1b7b57ad3e
Aman21-dev <devopsjava21@outlook.com>
2021-06-10 16:56:54 +0530

    modified controller

--- src/main/java/com/azure/devops/controller/SimpleController.java
+++ src/main/java/com/azure/devops/controller/SimpleController.java
@@ -8,7 +8,7 @@ public class SimpleController {
 	
 	@GetMapping
 	  public String hello() {
-	    return "Hello World i'm build in Azure Pipelines!";
+	    return "Hello World i'm build in Azure Pipeline CICD!";
 	  }
 
 }
