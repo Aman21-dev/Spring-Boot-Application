8488f4e218cf21555ad0f8bd49731aacb8c635aa
Aman21-dev <85228238+Aman21-dev@users.noreply.github.com>
2023-02-04 15:09:42 +0530

    Update SimpleController.java

--- src/main/java/com/azure/devops/controller/SimpleController.java
+++ src/main/java/com/azure/devops/controller/SimpleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SimpleController {
 	
-	@GetMapping("/hello")
+	@GetMapping("/satyajeet")
 	  public String hello() {
 	    return "Hello Satyajeet buy ADANI STOCKS !";
 	  }
